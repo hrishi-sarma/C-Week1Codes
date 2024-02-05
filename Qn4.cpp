8891ea8b789ab6f47c59a0ffc55d083b8654f859
Hrishi <70897209+hrishi-sarma@users.noreply.github.com>
2024-02-05 11:37:28 +0530

    Create Qn4.cpp

---
+++ Qn4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  int a;
+  std::cout << "Enter the number: ";
+  std::cin >> a;
+  std::cout << a << " x 1 = " << a*1 << std::endl << a << " x 2 = " << a*2 << std::endl << a << " x 3 = " << a*3 << std::endl << a << " x 4 = " << a*4 << std::endl << a << " x 5 = " << a*5 << std::endl << a << " x 6 = " << a*6 << std::endl << a << " x 7 = " << a*7 << std::endl << a << " x 8 = " << a*8 << std::endl << a << " x 9 = " << a*9 << std::endl << a << " x 10 = " << a*10 << std::endl;
+}
